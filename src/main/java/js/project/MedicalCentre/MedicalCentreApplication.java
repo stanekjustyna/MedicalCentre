a035abe7da6dfe4dab18e5261392bb9b0555e0f7
stanekjustyna <justynka.stanek.js@gmail.com>
2020-01-07 18:26:57 +0100

    initial commit

---
+++ src/main/java/js/project/MedicalCentre/MedicalCentreApplication.java
@@ -0,0 +1,13 @@
+package js.project.MedicalCentre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalCentreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalCentreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/js/project/MedicalCentre/MedicalCentreApplicationTests.java
@@ -0,0 +1,13 @@
+package js.project.MedicalCentre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalCentreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
